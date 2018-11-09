d3230bb6e038c21ab4ac4036c0b05e3b41e8e455
Ryan Maus <rmaus@id.iit.edu>
2018-11-08 23:10:26 -0600

    my first try

--- script.js
+++ script.js
@@ -1,100 +1,21 @@
-let box = document.getElementById("box-1"); 
+let apiKey = '0ba2a9b4f042b827d0dc4b09c98f99cb';
 
-const boxOnClick = (event) => {
-    let element = event.target; 
-    let color = element.style.backgroundColor; 
+let citySearch =  document.querySelector("#citySearch");
+let getWeatherNow = document.querySelector("#getWeatherNow");
+let getWeatherFiveDay = document.querySelector("#getWeatherFiveDay");
+let getWeatherIcon = document.querySelector("#getWeatherIcon");
 
-    if (color == "") {
-        element.style.backgroundColor = "red";
-    };
+const searchInput = (event) => {
+    let element = event.target;
+    let userInput = element.value;  
+
+    let URL = 'http://api.openweathermap.org/data/2.5/weather?q=+${userInput} + "&appid=" + apiKey;
+    
+    fetch(URL)
+    .then(  (response) => {return response.json(); } )
+    .then(getWeatherNow)
+    .then(getWeatherFiveDay)
+    .then(getWeatherIcon)
 }; 
 
-box.addEventListener("click", boxOnClick); 
-
-let box2 = document.getElementById("box-2"); 
-
-const boxOnDblClick = (event) => {
-    let element = event.target; 
-    let color = element.style.backgroundColor; 
-
-    if (color == "" || color == "green") {
-        element.style.backgroundColor = "blue";
-    } else if (color == "blue") {
-        element.style.backgroundColor = "green";
-    };
-}; 
-
-box2.addEventListener("dblclick", boxOnDblClick); 
-
-let box3 = document.getElementById("box-3"); 
-
-const boxOnMouseDown = (event) => {
-    let element = event.target; 
-    let color = element.style.backgroundColor; 
-
-    if (color == "" || color == "pink") {
-        element.style.backgroundColor = "orange";
-    };
-}; 
-
-box3.addEventListener("mousedown", boxOnMouseDown); 
-
-const boxOnMouseUp = (event) => {
-    let element = event.target; 
-    let color = element.style.backgroundColor; 
-
-    if (color == "orange") {
-        element.style.backgroundColor = "pink";
-    }; 
-}; 
-
-box3.addEventListener("mouseup", boxOnMouseUp);
-
-let box4 = document.getElementById("box-4"); 
-let box5 = document.getElementById("box-5"); 
-let box6 = document.getElementById("box-6"); 
-
-const boxOnMouseEnter = (event) => {
-    let element = event.target; 
-    let color = element.style.backgroundColor; 
-
-    if (color == "" || color == "teal") {
-        element.style.backgroundColor = "cyan";
-    };   
-}; 
-
-box4.addEventListener("mouseenter", boxOnMouseEnter);
-box5.addEventListener("mouseenter", boxOnMouseEnter);
-box6.addEventListener("mouseenter", boxOnMouseEnter);
-
-const boxOnMouseLeave = (event) => {
-    let element = event.target; 
-    let color = element.style.backgroundColor; 
-
-    if (color == "cyan") {
-        element.style.backgroundColor = "teal";
-    };  
-}; 
-
-box4.addEventListener("mouseleave", boxOnMouseLeave);
-box5.addEventListener("mouseleave", boxOnMouseLeave);
-box6.addEventListener("mouseleave", boxOnMouseLeave);
-
-let box7 = document.getElementById("box-7"); 
-let box8 = document.getElementById("box-8"); 
-let box9 = document.getElementById("box-9"); 
-
-const boxOnMouseMove = (event) => {
-    let element = event.target; 
-    let color = element.style.backgroundColor; 
-
-    if (color == "" || color == "gold") {
-        element.style.backgroundColor = "fuchsia";
-    } else if (color == "fuchsia") {
-        element.style.backgroundColor = "gold";
-    }  
-};
-
-box7.addEventListener("mousemove", boxOnMouseMove);
-box8.addEventListener("mousemove", boxOnMouseMove);
-box9.addEventListener("mousemove", boxOnMouseMove);
+citySearch.addEventListener("change", searchInput);
